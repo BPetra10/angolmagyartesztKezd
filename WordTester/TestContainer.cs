aeb6b0678a75415792518ff6013a896e3516d92a
Bodnár Petra <70892820+BPetra10@users.noreply.github.com>
2021-03-10 18:21:00 +0100

    v2 english-hungarian tester

---
+++ WordTester/TestContainer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordTester
+{
+    public class TestContainer
+    {
+        List<String> wordsPair;
+        List<String> RNDwordsPair;
+
+        public TestContainer(String fileName)
+        {
+            //kivételkezelés - állomány nem létezik?
+            try
+            {
+                wordsPair = File.ReadAllLines(fileName).ToList();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException($"A kapott {fileName} teszt nem létezik!");
+            }
+        }
+
+        public int Count => wordsPair.Count;
+
+        /// <summary>
+        /// A teljes szólistából adja vissza az angol szót
+        /// </summary>
+        /// <param name="n">A szó sorszáma. Számozás 0-tól indul</param>
+        /// <returns>Angol szó</returns>
+        public String GetWordUK(int n)
+        {
+            return wordsPair[n].Split(';')[0];
+        }
+
+
+        /// <summary>
+        /// A teljes szólistából adja vissza a magyar szót
+        /// </summary>
+        /// <param name="n">A szó sorszáma. Számozás 0-tól indul</param>
+        /// <returns>Magyar szó</returns>
+        public String GetWordHU(int n)
+        {
+            return wordsPair[n].Split(';')[1];
+        }
+        /// <summary>
+        /// Létrehoz egy véletlenszerű szólistát
+        /// </summary>
+        /// <param name="percent">Az eredeti lista hány %-a kerüljön a listába</param>
+        public void DoRandomListPercent(double percent)
+        {
+            //Hibavizsgálat!!!
+            DoRandomListNumber((int)(Math.Round(wordsPair.Count * percent / 100d)));
+        }
+        /// <summary>
+        /// Létrehoz egy véletlenszerű szólistát
+        /// </summary>
+        /// <param name="num">Hány szó kerüljön bele?</param>
+        public void DoRandomListNumber(int num)
+        {
+            //Hibavizsgálat!!!
+            Random rnd = new Random();
+            RNDwordsPair = new List<string>();
+            List<String> temp = wordsPair.Select(s => s).ToList();
+
+            for (int i = 0; i < num; i++)
+            {
+                int tempRND = rnd.Next(temp.Count);
+                RNDwordsPair.Add(temp[tempRND]);
+                temp.RemoveAt(tempRND);
+            }
+        }
+        /// <summary>
+        /// A véletlenszerűen leszűkített szólistából adja vissza az angol szót
+        /// </summary>
+        /// <param name="n">A szó sorszáma. Számozás 0-tól indul</param>
+        /// <returns>Angol szó</returns>
+        public String GetRandomWordUK(int n)
+        {
+            return RNDwordsPair[n].Split(';')[0];
+        }
+        /// <summary>
+        /// A véletlenszerűen leszűkített szólistából adja vissza a magyar szót
+        /// </summary>
+        /// <param name="n">A szó sorszáma. Számozás 0-tól indul</param>
+        /// <returns>Magyar szó</returns>
+        public String GetRandomWordHU(int n)
+        {
+            return RNDwordsPair[n].Split(';')[1];
+        }
+
+        public int RandomCount => RNDwordsPair.Count;
+
+    }
+}
---
+++ WordTester/TestWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WordTester
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        int currentQuestionNumber;
+        int numOfCorrectQuestion;
+        TestContainer actTest;
+        bool ISfromUKtoHU, IShelpForEveryWord;
+
+        public TestWindow(TestContainer par, bool ISfromUKtoHU, bool IShelpForEveryWord)
+        {
+            InitializeComponent();
+            this.ISfromUKtoHU = ISfromUKtoHU;
+            this.IShelpForEveryWord = IShelpForEveryWord;
+            this.actTest = par;
+            currentQuestionNumber = 0;
+            numOfCorrectQuestion = 0;
+            NextQuestion();
+            pbProcesss.Minimum = 0;
+            pbProcesss.Maximum = actTest.RandomCount + 1;
+
+        }
+
+        private void NextQuestion()
+        {
+            pbProcesss.Value++;
+            if (currentQuestionNumber == actTest.RandomCount)
+            {
+                grCells.IsEnabled = false;
+                MessageBox.Show($"Vége! Az eredménye {numOfCorrectQuestion} jó válasz a {currentQuestionNumber} kérdésből");
+                grCells.IsEnabled = true;
+                this.Close();
+                return;
+            }
+            lblQuestion.Content = ISfromUKtoHU ? actTest.GetRandomWordUK(currentQuestionNumber) : actTest.GetRandomWordHU(currentQuestionNumber);
+            currentQuestionNumber++;
+
+            txtAnswer.Focus();
+        }
+
+        private void btnAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            AnswerCheck();
+        }
+
+        private void AnswerCheck()
+        {
+            if (ISfromUKtoHU && actTest.GetRandomWordHU(currentQuestionNumber - 1) == txtAnswer.Text
+                || !ISfromUKtoHU && actTest.GetRandomWordUK(currentQuestionNumber - 1) == txtAnswer.Text)
+            {
+                numOfCorrectQuestion++;
+            }
+            else
+            {
+                if (IShelpForEveryWord)
+                {
+                    MessageBox.Show($"Téves! A helyes válasz: {(ISfromUKtoHU ? actTest.GetRandomWordHU(currentQuestionNumber - 1) : actTest.GetRandomWordUK(currentQuestionNumber - 1))}");
+                }
+            }
+            lblActResult.Content = Math.Round(100d * numOfCorrectQuestion / currentQuestionNumber) + "%";
+
+            txtAnswer.Text = "";
+            NextQuestion();
+        }
+
+        private void txtAnswer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                AnswerCheck();
+            }
+        }
+    }
+}
